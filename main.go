cc536e700c5438bb262c92a00cdbeca9473ff2c4
gmb0028 <girish.bhat@walmart.com>
2021-11-13 18:29:17 +0530

    added models

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"library-service/model"
+)
 
 func main() {
 	fmt.Println("hello library service")
+	books := []model.Book{}
+	book1 := model.Book{Id: "1", Name: "Harry Potter", Author: model.Author{Name: "Unknown"}}
+	books = append(books, book1)
+	books = append(books, book1)
+	books = append(books, book1)
+	library := model.Library{Books: books}
+	fmt.Printf("%+v\n", library)
+	fmt.Printf("%d", len(books))
+
 }
---
+++ model/library.go
@@ -0,0 +1,5 @@
+package model
+
+type Library struct {
+	Books []Book
+}
