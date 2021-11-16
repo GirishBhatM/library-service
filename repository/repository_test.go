ae7324d2390cf0ff2bdd147a9d887864e0afa48c
gmb0028 <girish.bhat@walmart.com>
2021-11-16 14:18:35 +0530

    added unit tests

---
+++ repository/repository_test.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"library-service/model"
+	"testing"
+)
+
+func TestSaveBook(t *testing.T) {
+	book := model.Book{Name: "Sample Book"}
+	bookId := Save(book)
+	if bookId != 1 {
+		t.Errorf("expected %d found %d", 1, bookId)
+	}
+}
+
+func TestGetById(t *testing.T) {
+	book := model.Book{Name: "Sample Book"}
+	bookId := Save(book)
+	_, exists := GetById(bookId)
+	if !exists {
+		t.Errorf("No book found for id %d", bookId)
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	Save(model.Book{Name: "Sample Book"})
+	Save(model.Book{Name: "Sample Book2"})
+	books := FindAll()
+	length := len(books)
+	if length != 4 {
+		t.Errorf("expected book size is not matching, expected %d got %d", 4, length)
+	}
+}
